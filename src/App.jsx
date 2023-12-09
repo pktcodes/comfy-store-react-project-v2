e30db928a7080a2d92904623f866763895c358f5
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-09 01:42:01 -0600

    DaisyUI Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="capitalize p-6 font-bold text-center underline text-3xl">
+    <h1 className="capitalize p-6 font-bold text-center underline text-3xl prose lg:prose-xl">
       Hello world!
     </h1>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import tailwindcssTypography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindcssTypography, daisyui],
 };
