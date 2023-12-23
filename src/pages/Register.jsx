28cf657ca5de1decb8d554ea5977f7a38684dc5c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-22 19:11:25 -0600

    Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,34 @@
+import { Form, Link } from "react-router-dom";
+
+import { FormInput, SubmitButton } from "../components";
+
 const Register = () => {
   return (
-    <div className="p-6 font-bold text-center underline text-4xl">Register</div>
+    <section className="grid h-screen place-items-center">
+      <Form
+        method="POST"
+        className="card  w-96 gap-y-4 bg-base-100 p-8 shadow-lg"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput label="username" type="text" name="username" />
+        <FormInput label="email" type="email" name="email" />
+        <FormInput label="password" type="password" name="password" />
+        <div className="mt-4">
+          <SubmitButton text="register" />
+        </div>
+        <p className=" text-center">
+          Already a member?
+          <span className="ml-2">
+            <Link
+              to="/login"
+              className="link-hover link capitalize text-primary"
+            >
+              login
+            </Link>
+          </span>
+        </p>
+      </Form>
+    </section>
   );
 };
 
