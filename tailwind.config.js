8c8d3adc4b55405ef2de7588eb233344ec0a5b65
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-23 22:15:43 -0600

    Add Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-import tailwindcssTypography from '@tailwindcss/typography';
-import daisyui from 'daisyui';
+import tailwindcssTypography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [tailwindcssTypography, daisyui],
+  daisyui: {
+    themes: ["dracula", "winter"],
+  },
 };
