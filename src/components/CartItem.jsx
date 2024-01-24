44b0615ad4dd5dcaf5825eaad3de4d0ddd88b502
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-01-23 23:52:48 -0600

    CartItems - Setup

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,58 @@
-const CartItem = () => {
-  return <h1 className="text-3xl">Cart Item</h1>;
+import { formatPrice, generateAmountInputs } from "../utils";
+
+const CartItem = ({ cartItem }) => {
+  console.log(cartItem);
+  const { cartID, image, title, company, productColor, amount, price } =
+    cartItem;
+
+  return (
+    <article className="mb-12 flex flex-col flex-wrap gap-y-4 border-b border-base-300 pb-6 last:border-b-0 sm:flex-row">
+      {/* IMAGE */}
+      <img
+        src={image}
+        alt={title}
+        className="h-24 w-24 rounded-lg object-cover sm:h-32 sm:w-32"
+      />
+      {/* INFO */}
+      <div className="sm:ml-16 sm:w-48">
+        {/* TITLE */}
+        <h3 className="font-medium capitalize">{title}</h3>
+        {/* COMPANY */}
+        <h4 className="mt-2 text-sm capitalize text-neutral-content">
+          {company}
+        </h4>
+        {/* COLOR */}
+        <p className="mt-4 flex items-center gap-x-2 text-sm capitalize">
+          color:
+          <span
+            className="badge badge-sm"
+            style={{ backgroundColor: productColor }}
+          ></span>
+        </p>
+      </div>
+      <div className="sm:ml-24">
+        {/* AMOUNT */}
+        <div className="form-control max-w-xs">
+          <label htmlFor="amount" className="label p-0">
+            <span className="label-text">Amount</span>
+          </label>
+          <select
+            id="amount"
+            name="amount"
+            className="select select-bordered select-ghost select-xs mt-2"
+          >
+            {generateAmountInputs(amount + 5)}
+          </select>
+        </div>
+        {/* REMOVE */}
+        <button className="link-hover link link-primary mt-2 text-sm">
+          remove
+        </button>
+      </div>
+      {/* PRICE */}
+      <p className="font-medium sm:ml-auto">{formatPrice(price)}</p>
+    </article>
+  );
 };
 
 export default CartItem;
--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
+import CartItem from "./CartItem";
+
 const CartItemsList = () => {
-  return <h1 className="text-3xl">CartItemsList</h1>;
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
+  return (
+    <>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </>
+  );
 };
 
 export default CartItemsList;
