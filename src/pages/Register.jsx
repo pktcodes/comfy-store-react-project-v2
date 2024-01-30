4282ef1d2cb6657aa5ef01fe824626a955601f6e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-01-29 21:12:21 -0600

    Register User - Complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,25 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import { FormInput, SubmitButton } from "../components";
+import { customFetch } from "../utils";
 
-export const action = () => {
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const inputData = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post("/auth/local/register", inputData);
+    console.log(response);
+    toast.success("Account created successfully");
+    return redirect("/login");
+  } catch (error) {
+    console.log(error);
+    const errorMessage =
+      error?.response?.data?.error?.message || "Please verify the credentials";
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
