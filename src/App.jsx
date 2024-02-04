62bcd0b674265e2be87fc4560e602e0a4d7c4f62
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-02-03 21:32:57 -0600

    Checkout Page - Restrict Access

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
+import { loader as checkoutLoader } from "./pages/Checkout";
 
 /* Actions */
 import { action as registerAction } from "./pages/Register";
@@ -50,6 +51,7 @@ const router = createBrowserRouter([
       {
         path: "checkout",
         element: <Checkout />,
+        loader: checkoutLoader(store),
       },
       {
         path: "products",
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,18 @@
 import { useSelector } from "react-redux";
+import { redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import { CartTotals, CheckoutForm, SectionTitle } from "../components";
+
+export const loader = (store) => () => {
+  const user = store.getState().userState.user;
+
+  if (!user) {
+    toast.warn("You must be logged in to checkout");
+    return redirect("/login");
+  }
+  return null;
+};
 
 const Checkout = () => {
   const numberOfItemsInCart = useSelector(
