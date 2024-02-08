09044da8ac66ca841dcb868573bbe3d4e84627ae
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-02-07 23:57:09 -0600

    Orders Page - Loader Setup

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,22 @@
+import { redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export const loader = (store) => async () => {
+  const { user } = store.getState().userState;
+
+  if (!user) {
+    toast.warn(
+      "You must be logged in to view ordersPlease login to access orders",
+    );
+    return redirect("/login");
+  }
+
+  return null;
+};
+
 const Orders = () => {
   return (
-    <div className="p-6 font-bold text-center underline text-4xl">Orders</div>
+    <div className="p-6 text-center text-4xl font-bold underline">Orders</div>
   );
 };
 
